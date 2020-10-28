808af5b9868936a1924ef950a21bc8689bf8816b
Jeff Ashton <Jeff.Ashton@D2L.com>
2020-10-28 17:21:11 -0400

    Writing docs

--- README.md
+++ README.md
@@ -1,3 +1,63 @@
 # dotnet-dynamodb-lock
 
 Dotnet tool for acquiring/releasing locks in a DynamoDB table.
+
+## Usage
+
+### Acquire
+
+```
+dotnet dynamodb-lock acquire --config <configFile> --label <label> --output <tokenFile>
+```
+
+The label is used to provide context to others trying to acquire the lock
+
+```
+> dotnet dynamodb-lock acquire --config lock.config --label 'Build 123' --output token.txt
+
+Locked by 'Build 122' at 8:50:43 PM. Expires at 9:20:43 PM.
+```
+
+### Release
+
+```
+dotnet dynamodb-lock release --config <configFile> --token <tokenFile>
+```
+
+The token file is the output of the acquire command.
+
+```
+> dotnet dynamodb-lock release --config lock.config --token token.txt
+
+Lock released.
+```
+
+### Config
+
+```
+	{
+		"tableName": "locks",
+		"lockKey": "build",
+
+		"awsRegion": "us-east-1",
+		"roleArn": "arn:aws:iam::111111111111:role/build"
+
+		"acquireTimeout":300,
+		"lockDuration":1800,
+		"retryAfter":5,
+	}
+```
+
+`tableName:` The DynamoDB table name **\[required\]**
+
+`lockKey:` The unique key for the lock **\[required\]**
+
+`awsRegion`: The aws region of the table *\[optional\]*
+
+`roleArn`: The arn of the role to assume *\[optional\]*
+
+`acquireTimeout`: How long to wait to acquire the lock before timing out *\[optional\]*
+
+`lockDuration`: How long the lock is valid for *\[optional\]*
+
+`retryAfter`: How long to wait inbetween retries to acquire the lock *\[optional\]*
--- src/DotnetDynamoDBLock/Config/LockConfig.cs
+++ src/DotnetDynamoDBLock/Config/LockConfig.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Text.Json.Serialization;
 
 namespace DotnetDynamoDBLock.Config {
 
 	internal sealed class LockConfig {
 
-		[JsonPropertyName( "awsRegion" )]
-		public string? AwsRegion { get; set; }
+		public static void WriteUsage( IndentedTextWriter writer ) {
+
+			writer.WriteLine( "Config:" );
+			writer.Indent++;
+			writer.WriteLine();
+			{
+				writer.WriteLine( "{" );
+				writer.Indent++;
+				{
+					writer.WriteLine( "\"tableName\": \"locks\"," );
+					writer.WriteLine( "\"lockKey\": \"build\"," );
+					writer.WriteLine();
+
+					writer.WriteLine( "\"awsRegion\": \"us-east-1\"," );
+					writer.WriteLine( "\"roleArn\": \"arn:aws:iam::111111111111:role/build\"" );
+					writer.WriteLine();
+
+					writer.WriteLine( "\"acquireTimeout\":300," );
+					writer.WriteLine( "\"lockDuration\":1800," );
+					writer.WriteLine( "\"retryAfter\":5," );
+
+				}
+				writer.Indent--;
+				writer.WriteLine( "}" );
+			}
+
+			writer.WriteLine();
+			writer.WriteLine( "tableName:         The DynamoDB table name                                   [required]" );
+			writer.WriteLine( "lockKey:           The unique key for the lock                               [required]" );
+			writer.WriteLine();
+
+			writer.WriteLine( "awsRegion:         The aws region of the table                               [optional]" );
+			writer.WriteLine( "roleArn:           The arn of the role to assume                             [optional]" );
+			writer.WriteLine();
+
+			writer.WriteLine( "acquireTimeout:    How long to wait to acquire the lock before timing out    [optional]" );
+			writer.WriteLine( "lockDuration:      How long the lock is valid for                            [optional]" );
+			writer.WriteLine( "retryAfter:        How long to wait inbetween retries to acquire the lock    [optional]" );
+			writer.WriteLine();
+
+			writer.Indent--;
+			writer.WriteLine();
+		}
 
 		[JsonPropertyName( "tableName" )]
 		public string TableName { get; set; } = string.Empty;
@@ -14,14 +56,25 @@ namespace DotnetDynamoDBLock.Config {
 		[JsonPropertyName( "lockKey" )]
 		public string LockKey { get; set; } = string.Empty;
 
+		[JsonPropertyName( "awsRegion" )]
+		public string? AwsRegion { get; set; }
+
 		[JsonPropertyName( "roleArn" )]
 		public string? RoleArn { get; set; }
+
+		[JsonPropertyName( "acquireTimeout" )]
+		public int AcquireTimeoutSeconds { get; set; } = 300;
 
 		[JsonPropertyName( "lockDuration" )]
 		public int LockDurationSeconds { get; set; } = 1800;
 
 		[JsonPropertyName( "retryAfter" )]
 		public int RetryAfterSeconds { get; set; } = 5;
+
+		[JsonIgnore]
+		public TimeSpan AcquireTimeout {
+			get { return TimeSpan.FromSeconds( AcquireTimeoutSeconds ); }
+		}
 
 		[JsonIgnore]
 		public TimeSpan LockDuration {
